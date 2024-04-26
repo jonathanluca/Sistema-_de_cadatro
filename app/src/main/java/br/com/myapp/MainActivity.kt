e5caa461e7987cec76f9b264bd19cfe739034e69
unknown <jonathanlsantos.m@gmail.com>
2024-04-26 15:46:30 -0300

    Criação de algumas telas

--- app/src/main/java/br/com/myapp/MainActivity.kt
+++ app/src/main/java/br/com/myapp/MainActivity.kt
@@ -1,20 +1,28 @@
 package br.com.myapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.com.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navEntreTelas()
+    }
+    private fun navEntreTelas() {
+        binding.buttonAcessar.setOnClickListener {
+            val Tela_incial = Intent(this, Tela_incial::class.java)
+            startActivity(Tela_incial)
+        }
+        binding.RedefinirSenha.setOnClickListener {
+            val Redefinir_senha = Intent(this, Redefinir_senha::class.java)
+            startActivity(Redefinir_senha)
         }
     }
 }
