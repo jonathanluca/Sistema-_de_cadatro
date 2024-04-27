c955b7800f7efc611aff6689d3290783d24c2f07
unknown <jonathanlsantos.m@gmail.com>
2024-04-26 23:03:44 -0300

    Autenticação da tela de login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,12 +40,14 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/br/com/myapp/MainActivity.kt
+++ app/src/main/java/br/com/myapp/MainActivity.kt
@@ -3,22 +3,51 @@ package br.com.myapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.myapp.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navEntreTelas()
+
+    }
+
+    private fun autenticar(email:String, senha:String){
+        auth.signInWithEmailAndPassword(email,senha).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                Toast.makeText(baseContext,"Autenticado", Toast.LENGTH_LONG)
+
+            }
+        }
     }
     private fun navEntreTelas() {
         binding.buttonAcessar.setOnClickListener {
-            val Tela_incial = Intent(this, Tela_incial::class.java)
-            startActivity(Tela_incial)
+
+            auth = Firebase.auth
+            val email = findViewById<EditText>(R.id.edit_email).text.toString()
+            val senha = findViewById<EditText>(R.id.edit_senha).text.toString()
+
+            if (email.isNotEmpty()&&senha.isNotEmpty()) {
+                autenticar(email,senha)
+                val Tela_incial = Intent(this, Tela_incial::class.java)
+                startActivity(Tela_incial)
+            }
+
         }
         binding.RedefinirSenha.setOnClickListener {
             val Redefinir_senha = Intent(this, Redefinir_senha::class.java)
